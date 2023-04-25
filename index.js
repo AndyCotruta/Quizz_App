f851da0d08d526de7b398540a08f28d02b1d73f4
AndyCotruta <acotruta18@gmail.com>
2023-04-25 22:09:57 +0200

    responsive design for index done

--- index.js
+++ index.js
@@ -3,11 +3,18 @@ let btn = document.getElementById("inactivebtn");
 let anchorTag = document.getElementById("anchorTag");
 
 check.addEventListener("click", () => {
-  console.log(`the button is ${btn}`);
-  btn.style.backgroundColor = "#01ffff";
-  anchorTag.classList.remove("disabled");
-  btn.classList.remove("disabled");
-  btn.classList.add("enabledbtn");
+  // if (btn.classList === "disabled") {
+  if (btn.classList.toString() === "disabled") {
+    btn.style.backgroundColor = "#01ffff";
+    anchorTag.classList.remove("disabled");
+    btn.classList.remove("disabled");
+    btn.classList.add("enabledbtn");
+  } else if (btn.classList.toString() !== "disabled") {
+    btn.style.backgroundColor = "#1daabb";
+    anchorTag.classList.add("disabled");
+    btn.classList.add("disabled");
+    btn.classList.remove("enabledbtn");
+  }
 });
 
 // const isChecked = () => {
